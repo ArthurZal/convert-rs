dafac7026d3c4862986a47e0a725ec1ec835d262
Arthur Zall <zallarthur@gmail.com>
2024-08-15 20:05:13 -0300

    Obtendo o valor da moeda selecionada para a correção

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-
+const form = document.querySelector("form");
 const inputAmount = document.querySelector("input");
+const currency = document.querySelector("#currency");
 
 inputAmount.addEventListener("input", () => {
 
     const hasCharactersRegex = /\D+/g
 
     inputAmount.value = inputAmount.value.replace(hasCharactersRegex, "")
-    
-    console.log(inputAmount.value);
-});
+
+    //console.log(inputAmount.value)
+})
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    console.log(currency.value)
+}
 

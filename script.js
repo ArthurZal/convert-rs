8162ff97da14ed35a670bbbd435a0c27cfcc763d
Arthur Zall <zallarthur@gmail.com>
2024-08-15 19:54:04 -0300

    Adicionando tratativa com expressões regulares no input para receber apenas dados do tipo number

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const inputAmount = document.querySelector("input");
 
 inputAmount.addEventListener("input", () => {
+
+    const hasCharactersRegex = /\D+/g
+
+    inputAmount.value = inputAmount.value.replace(hasCharactersRegex, "")
+    
     console.log(inputAmount.value);
-})
+});
 
